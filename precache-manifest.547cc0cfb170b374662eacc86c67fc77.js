7a2233cb4a88ef0e6d0ca668bd82003107ad3146
Palekha Kostantin <swotwor@gmail.com>
2023-01-25 04:45:38 +0200

    First commit

---
+++ precache-manifest.547cc0cfb170b374662eacc86c67fc77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ad0b5339f5118178954",
+    "url": "/static/js/main.b0b3f69b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0364ec6626b709af407b",
+    "url": "/static/js/2.af1cbaa4.chunk.js"
+  },
+  {
+    "revision": "c3183b00990bc14922e0834578cf953d",
+    "url": "/static/media/termsIcon.c3183b00.svg"
+  },
+  {
+    "revision": "29665739d6ae3751a048b15865118daa",
+    "url": "/index.html"
+  }
+];
